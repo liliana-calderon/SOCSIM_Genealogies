f149baf40d2b0ee3fbb265715b0369543067d750
Calderon Bernal <calderonbernal@demogr.mpg.de>
2023-05-25 17:58:11 +0200

    Delete read_opop and read_omar functions that are now integrated in the package

--- read_omar.R
+++
@@ -1,8 +0,0 @@
-## Function written by Diego Alburez-Gutierrez. 
-# This will be later integrated in the rsocsim package
-
-read_omar <- function(path){
-  omar<-read.table(file = path, header = F, as.is = T)
-  names(omar)<-c("mid","wpid","hpid","dstart","dend", "rend","wprior","hprior")
-  return(omar)
-}
