25dba6ad7eeccb90ac6b805c6807db2f547489ce
Calderon Bernal <calderonbernal@demogr.mpg.de>
2024-09-02 15:23:19 +0200

    Adjust simulation details to supfile until 2072

--- 1_Run_Simulations.R
+++ 1_Run_Simulations.R
@@ -2,11 +2,11 @@
 # SOCSIM - SOCSIM Genealogies - Run simulations 
 # U:/SOCSIM/SOCSIM_Genealogies/1_Run_Simulations.R
 
-## Run 10 SOCSIM demographic microsimulation using the 'rsocsim' package and data for Sweden (1751-2022)
+## Run 10 SOCSIM demographic microsimulation using the 'rsocsim' package and data for Sweden (1751-2072)
 ## and read the output into R. 
 
 # Created on 18-01-2022
-# Last modified on 28-08-2023
+# Last modified on 02-09-2024
 #------------------------------------------------------------------------------------------------------
 # SOCSIM simulation: rate files and assumptions ----
 
@@ -30,6 +30,8 @@
 
 # The first segment of the simulation runs for 100 years to produce a stable age structure, 
 # based on 1751-HFC and 1751-HMD age-specific rates
+# The last segment of the simulation runs for another 100 years to avoid problems with the final population,
+# based on 2022 HFD and HMD age-specific rates
 
 #------------------------------------------------------------------------------------------------------
 ## Run SOCSIM simulations with 'rsocsim' package ----
@@ -53,15 +55,15 @@ folder <- getwd()
 # Otherwise, the working directory must be specified after "folder <- "
 
 # Name of the supervisory file stored in the above folder:
-supfile <- "Sweden_0.sup"
-# Sup file for rates retrieved from HFC/HFD and HMD (1751-2022), created with the 0_Write_Input_Rates.R,  
+supfile <- "Sweden_0_72.sup"
+# Sup file for rates retrieved from HFC/HFD and HMD (1751-2072), created with the 0_Write_Input_Rates.R,  
 # using marry after childbirth directive (heterogeneous fertility disabled)
 
 # Random number generator seed:
 sims_seeds <- as.character(sample(1:99999, 10, replace = F))
 
 # NB: To get exactly the same simulation results, you should use the same randomly generated seeds
-# sims_seeds <- c("67926", "22403", "36602", "24856", "74711", "38132", "21702", "93981", "23429", "82601")
+# sims_seeds <- c("51972", "16372", "47533", "10973", "52743", "23235", "75025", "46101", "29373", "76946")
 
 # Save the seeds numbers to use them later to read the data
 save(sims_seeds, file = "sims_seeds.Rda")
@@ -70,14 +72,13 @@ save(sims_seeds, file = "sims_seeds.Rda")
 start <- Sys.time()
 for(seed in sims_seeds) {
   
-  ### Run a single SOCSIM-simulation with a given folder and supervisory file,
-  # using the "clustercall" process method, which allow to run several simulations. 
+  ### Run a single SOCSIM-simulation with a given folder, seed and supervisory file
   rsocsim::socsim(folder, supfile, seed, process_method = "future")
   
 }
 end <- Sys.time()
 print(end-start)
-# Time difference of 3.616023 hours for  10 simulations, with initial population of 5000 and hetfert 0
+# Time difference of 4.39479 hours for 10 simulations (1651-2072), with initial population of 5000 and hetfert 0 
 #----------------------------------------------------------------------------------------------------
 ## Read the output .opop and .omar files ----
 
@@ -90,7 +91,7 @@ sims_seeds <- as.numeric(sims_seeds)
 
 # Iterate the function for the 10 seeds to read opop of the 10 simulations
 sims_opop <- map(sims_seeds, ~ rsocsim::read_opop(folder = getwd(),
-                                                  supfile = "Sweden_0.sup",
+                                                  supfile = "Sweden_0_72.sup",
                                                   seed = .,
                                                   suffix = "",
                                                   fn = NULL))
@@ -104,7 +105,7 @@ save(sims_opop, file = "sims_opop.RData")
 
 # Iterate the function for the 10 seeds to read omar of the 10 simulations
 sims_omar <- map(sims_seeds, ~ rsocsim::read_omar(folder = getwd(),
-                                                  supfile = "Sweden_0.sup",
+                                                  supfile = "Sweden_0_72.sup",
                                                   seed = .,
                                                   suffix = "",
                                                   fn = NULL))
